cde4506f9fe7eb9a5c93b6cd5516e65706c4e701
Jacob Hsu <jacobhsu@dopod.com>
2015-09-09 17:26:00 +0800

    http://php.net/manual/en/function.curl-setopt.php CURLOPT_RETURNTRANSFER, CURLOPT_USERAGENT, CURLOPT_SSL_VERIFYPEER

--- php.net/curl_setopt.php
+++ php.net/curl_setopt.php
@@ -1,14 +1,21 @@
 <?php
+$url = 'https://www.microsoft.com/zh-tw/store/apps/facebook/9wzdncrfhv5g';
+
 // create a new cURL resource
 $ch = curl_init();
 
 // set URL and other appropriate options
-curl_setopt($ch, CURLOPT_URL, "http://www.example.com/");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //TRUE to return the raw output when CURLOPT_RETURNTRANSFER is used.
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)"); //The contents of the "User-Agent: " header to be used in a HTTP request.
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //FALSE to stop cURL from verifying the peer's certificate.
 
 // grab URL and pass it to the browser
-curl_exec($ch);
+$html = curl_exec($ch);
 
 // close cURL resource, and free up system resources
 curl_close($ch);
+
+var_dump($html);
 ?>
