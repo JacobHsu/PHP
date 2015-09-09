f2cf71018b773706128b52aeb5a76de422be9fad
Jacob Hsu <jacobhsu@dopod.com>
2015-09-09 18:04:06 +0800

    http://php.net/manual/en/function.strstr.php

--- php.net/curl_setopt.php
+++ php.net/curl_setopt.php
@@ -1,21 +1,27 @@
 <?php
-$url = 'https://www.microsoft.com/zh-tw/store/apps/facebook/9wzdncrfhv5g';
+    $url = 'https://www.microsoft.com/zh-tw/store/apps/facebook/9wzdncrfhv5g';
 
-// create a new cURL resource
-$ch = curl_init();
+    // create a new cURL resource
+    $ch = curl_init();
 
-// set URL and other appropriate options
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //TRUE to return the raw output when CURLOPT_RETURNTRANSFER is used.
-curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)"); //The contents of the "User-Agent: " header to be used in a HTTP request.
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //FALSE to stop cURL from verifying the peer's certificate.
+    // set URL and other appropriate options
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //TRUE to return the raw output when CURLOPT_RETURNTRANSFER is used.
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)"); //The contents of the "User-Agent: " header to be used in a HTTP request.
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //FALSE to stop cURL from verifying the peer's certificate.
 
-// grab URL and pass it to the browser
-$html = curl_exec($ch);
+    // grab URL and pass it to the browser
+    $html = curl_exec($ch);
 
-// close cURL resource, and free up system resources
-curl_close($ch);
+    // close cURL resource, and free up system resources
+    curl_close($ch);
 
-var_dump($html);
+
+    $data['name'] = tag_content($html, '<h1 id="page-title" class="header-small m-v-n" itemprop="name">', "</h1>");
+    function tag_content($html, $start, $end)
+    {
+        return strstr($html, $end, true); //html before </h1>
+    }
+    print_r($data); 
 ?>
